12659909813451ec189704a1661a739690248340
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-09-21 16:24:51 +0300

    Initial Commit

---
+++ 12.Hafta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12.Hafta
+//
+//  Created by Sümeyye Kılıçoğlu on 21.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
